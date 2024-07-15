f5a044fb263a9dc442b92c0e3b25614ef3dd6ac9
002ploki <002ploki@gmail.com>
2024-07-16 00:52:09 +1000

    16/07/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,139 +1,58 @@
 import React, {useState, useEffect} from 'react'
+import './appStyle.css'
 
 export default function App() {
-    const [numbs, setNumbs] = useState('');
-    const [equation, setEquation] = useState([]);
-    const [renew, setRenew] = useState(false);
+    const [nambas, setNambas] = useState([1,2,3,4,5,6,7,8,9,10,11,12]);
+    const sydneyTime = new Date().toLocaleString("en-US", {
+      timeZone: "Australia/Sydney",
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true
+    });
+    const [todayTime, setTodayTime] = useState(sydneyTime);
+    useEffect(()=>{
+        const intervalId = setInterval(() => {
+          const updatedTime = new Date().toLocaleString("en-US", {
+            timeZone: "Australia/Sydney",
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true
+          });
+          setTodayTime(updatedTime);
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
 
-    function handleClickNumbers(value){
-            setNumbs((currentNumbs) => {
-                return currentNumbs + String(value)
-            })
-            if(renew === true){
-                setEquation([]);
-                setRenew(false);
-            }
-    }
-
-    function numbBackSpace(){
-        setNumbs((currentNumbs) =>{
-            return currentNumbs.slice(0, currentNumbs.length-1)
-        })
-    }
-
-    function equationBackSpace(){
-        setEquation((currentEquation) =>{
-            return [
-                ...currentEquation.slice(0, currentEquation.length-2)
-            ]
-        })
-    }
-
-    function handleClickOperator(value){
-        if(numbs.length === 0){
-            return
-        } else{
-            const tempNumbs = numbs;
-            setEquation((currentEquation) =>{
-                setNumbs('')
-                return [
-                    ...currentEquation,
-                    tempNumbs, value
-                ]
-            })
-        }
-    }
-
-    function handleSubmit(){
-        function operation(i){
-            const operator = arr[i];
-            const number1 = Number(arr[i - 1]);
-            const number2 = Number(arr[i + 1]);
-            arr.splice(i-1, 2);
-            if(operator === '+'){
-                arr[i-1] = number1 + number2;
-            } else if(operator === '-'){
-                arr[i-1] = number1 - number2;
-            } else if(operator === '*'){
-                arr[i-1] = number1 * number2;
-            } else if(operator === '/'){
-                arr[i-1] = number1 / number2;
-            } 
-        }
-        const arr = equation
-        arr.push(numbs);
-        while(true){
-            let operated = false;
-            for(let i = 0; i < arr.length; i++){
-                if (arr[i] === '*' || arr[i] ==='/'){
-                    operation(i);
-                    operated = true;
-                    break;
-                }
-            }
-            if (operated === false){
-                break;
-            }
-
-        }
-        while(true){
-            let operated = false;
-            for(let i = 0; i < arr.length; i++){
-                if (arr[i] === '+' || arr[i] ==='-'){
-                    operation(i);
-                    operated = true;
-                    break;
-                }
-            }
-            if (operated === false){
-                break;
-            }
-        }
-        const tempRes = arr[0];
-        setNumbs('');
-        setEquation([tempRes]);
-        setRenew(true);
-    }
 
   return (
     <>
-        <section className="screen">
-            <div className="screen__equation">
-                {equation.join('')+numbs}
-            </div>
-            <div className="screen__result">
-                {numbs}
-            </div>
-        </section>
-        <section className="numbers">
-            <div className="row">
-                <button onClick={() => handleClickNumbers(7)}>7</button>
-                <button onClick={() => handleClickNumbers(8)}>8</button>
-                <button onClick={() => handleClickNumbers(9)}>9</button>
-            </div>
-            <div className="row">
-                <button onClick={() => handleClickNumbers(4)}>4</button>
-                <button onClick={() => handleClickNumbers(5)}>5</button>
-                <button onClick={() => handleClickNumbers(6)}>6</button>
-            </div>
-            <div className="row">
-                <button onClick={() => handleClickNumbers(1)}>1</button>
-                <button onClick={() => handleClickNumbers(2)}>2</button>
-                <button onClick={() => handleClickNumbers(3)}>3</button>
-            </div>
-            <div className="row">
-                <button onClick={() => handleClickNumbers(0)}>0</button>
-                <button onClick={() => numbBackSpace()}>b</button>
-                <button onClick={() => equationBackSpace()}>cl</button>
-            </div>
-            <div className="row">
-                <button onClick={() => handleClickOperator('+')}>+</button>
-                <button onClick={() => handleClickOperator('-')}>-</button>
-                <button onClick={() => handleClickOperator('/')}>/</button>
-                <button onClick={() => handleClickOperator('*')}>*</button>
-                <button onClick={() => handleSubmit()}>=</button>
-            </div>
-        </section>
+      <section>
+        {nambas.map(namba => (
+            <span key={namba}
+            className={`numba ${namba}`}
+                style={{
+                  transform: `rotate(calc(${namba} * 30deg)) translate(-50%, -50%)`
+                }}
+            >{namba}</span>
+        ))}
+          <div className="seconds"
+            style={{
+              transform:`rotate(calc(${todayTime.slice(6,8)} * 6deg)) translate(-50%, -50%)`
+            }}
+          ></div>
+          <div className="minutes"
+            style={{
+              transform:`rotate(calc(${todayTime.slice(3,5)} * 6deg)) translate(-50%, -50%)`
+            }}
+          ></div>
+          <div className="hours"
+            style={{
+            transform:`rotate(calc(${todayTime.slice(0,2)} * 30deg + ${todayTime.slice(3,5)} * 0.5deg)) translate(-50%, -50%)`
+          }}
+          ></div>
+      </section>
     </>
   )
 }
